76923dff6ab38f3dca75e3627cb407ef20b69cf5
Thibaud Derippe <Thibaud.Derippe@gmail.com>
2022-03-17 10:15:41 -0400

    V2 fig 3

--- figures.R
+++ figures.R
@@ -2089,22 +2089,16 @@ plot2 <-
   geom_segment(data = tibble(k2_1), aes(x = k2_1, xend = k2_1, y = min(lambda0_1), yend = max(lambda0_1)), lty = 2, alpha  = 0.3)+
   geom_segment(data = tibble(lambda0_1), aes(x = min(k2_1), xend = max(k2_1), y = lambda0_1, yend = lambda0_1), lty = 2, alpha  = 0.3)+
   # geom_point(data = ids %>% left_join(idoutput), aes(k2, lambda0))+
-  geom_point(data= self$filters_neg_above, aes(k2, lambda0, col = "Above"))+
-  geom_rect(data= self$filters_neg_above, aes(xmin = 0, xmax = k2, ymin = lambda0, ymax = Inf, fill = "Above", col = "Above"), alpha = 0.2)+
-  geom_rect(data= self$filters_neg_below, aes(xmin = k2, xmax = Inf, ymin = 0, ymax = lambda0, fill = "Below", col = "Below"), alpha = 0.2)+
-  geom_rect(data = maybe,aes(xmin = k2min, xmax = k2max, ymin = lambda0min, ymax = lambda0max, fill = "To explore" ), alpha = 0.6)+
+
+  geom_rect(data= self$filters_neg_above, aes(xmin = 0, xmax = k2, ymin = lambda0, ymax = Inf, fill = "Above", col = "Above"), alpha = 1)+
+  geom_rect(data= self$filters_neg_below, aes(xmin = k2, xmax = Inf, ymin = 0, ymax = lambda0, fill = "Below", col = "Below"), alpha = 1)+
+  geom_rect(data = maybe,aes(xmin = k2min, xmax = k2max, ymin = lambda0min, ymax = lambda0max, fill = "To explore" ), alpha = 0.6,  col = "black")+
   scale_x_continuous(breaks = k2_1)+
   scale_y_continuous(breaks = lambda0_1)+
-  geom_segment(aes(x = 1.2, xend = 1.6, y = 0.8, yend = 0.8))+
-  geom_segment(aes(x = 1.2, xend = 1.6, y = 1, yend = 1))+
-  geom_segment(aes(x = 1.6, xend = 1.6, y = 0.8, yend = 1))+
-  geom_segment(aes(x = 1.2, xend = 1.2, y = 0.8, yend = 1))+
-  geom_segment(data = tibble(k2_2), aes(x = k2_2, xend = k2_2, y = min(lambda0_2), yend = max(lambda0_2)), lty = 3)+
-  geom_segment(data = tibble(lambda0_2), aes(x = min(k2_2), xend = max(k2_2), y = lambda0_2, yend = lambda0_2), lty = 3)+
-
-
+  geom_text(data = maybe %>% rowid_to_column("id"), aes(x = (k2min + k2max) /2, y = (lambda0min + lambda0max)/2, label = id) )+
   geom_segment(aes(x = 0, y = 0, xend = Inf, yend = 0, col = "Below"))+
   geom_point(data= self$filters_neg_below, aes(k2, lambda0, col = "Below"))+
+  geom_point(data= self$filters_neg_above, aes(k2, lambda0, col = "Above"))+
   scale_color_manual(values = c("red", "chocolate"))+
   scale_fill_manual(values = c("red", "chocolate", "grey"))+
   labs(col = "", fill = "", x = "K2", y = "lambda0")+
@@ -2112,11 +2106,256 @@ plot2 <-
   guides(col = F)+
   theme(line = element_blank()); plot2
 
+plot_grid(plot1, plot2)
+
+digit <- 3
+
+maybe %>% slice(1) -> x
 
 
-self2 <- VP_proj_creator$new()
+maybe %>%
+  rowid_to_column("id") %>%
+  group_split(id) %>%
+  map(function(x){
 
-#
+  difx <-  x$k2max -   x$k2min
+  dify <-  x$lambda0max -   x$lambda0min
+
+crossing(k2 = seq(x$k2min, x$k2max, difx/6) %>% round(3),
+         lambda0 = seq(x$lambda0min, x$lambda0max, dify/6) %>% round(3)) %>%
+  mutate(bloc = x$id)
+
+  }) %>%
+  bind_rows() -> newVPs
+
+newVPs %>%
+  left_join(
+
+    newVPs %>%
+      group_by(bloc) %>%
+      summarise(k2min = min(k2), k2max = max(k2), lambda0min = min(lambda0), lambda0max = max(lambda0))
+
+    ) -> forseg
+
+
+plot3 <- ggplot()+
+  geom_segment(data = forseg, aes(x = k2, xend = k2, y = lambda0, yend = lambda0max), lty = 3)+
+  geom_segment(data = forseg, aes(x = k2, xend = k2max, y = lambda0, yend = lambda0), lty = 3)+
+  # geom_segment(data = tibble(k2_1), aes(x = k2_1, xend = k2_1, y = min(lambda0_1), yend = max(lambda0_1)), lty = 2, alpha  = 0.3)+
+  # geom_segment(data = tibble(lambda0_1), aes(x = min(k2_1), xend = max(k2_1), y = lambda0_1, yend = lambda0_1), lty = 2, alpha  = 0.3)+
+  # geom_point(data = ids %>% left_join(idoutput), aes(k2, lambda0))+
+
+  geom_rect(data= self$filters_neg_above, aes(xmin = 0, xmax = k2, ymin = lambda0, ymax = Inf, fill = "Above", col = "Above"), alpha = 1)+
+  geom_rect(data= self$filters_neg_below, aes(xmin = k2, xmax = Inf, ymin = 0, ymax = lambda0, fill = "Below", col = "Below"), alpha = 1)+
+  geom_rect(data = maybe,aes(xmin = k2min, xmax = k2max, ymin = lambda0min, ymax = lambda0max, fill = "To explore"), alpha = 0.6,  col = "black")+
+  # scale_x_continuous(breaks = k2_1)+
+  # scale_y_continuous(breaks = lambda0_1)+
+   # geom_segment(aes(x = 0, y = 0, xend = Inf, yend = 0, col = "Below"))+
+  # geom_point(data= self$filters_neg_below, aes(k2, lambda0, col = "Below"))+
+  # geom_point(data= self$filters_neg_above, aes(k2, lambda0, col = "Above"))+
+  scale_color_manual(values = c("red", "chocolate"))+
+  scale_fill_manual(values = c("red", "chocolate", "grey"))+
+  labs(col = "", fill = "", x = "K2", y = "lambda0")+
+  theme_bw()+
+  guides(col = F)+
+
+  theme(line = element_blank())+
+  geom_point(data = newVPs, aes(k2, lambda0)); plot3
+  # geom_label(data = maybe %>% rowid_to_column("id"), aes(x = (k2min + k2max) /2, y = (lambda0min + lambda0max)/2, label = id), fill = "grey" )
+
+
+
+self2 <- self$clone(deep = T)
+
+newVPs %>%
+  mutate(forjoin = 1) %>%
+  left_join(
+VP_df %>% select(-k2, -lambda0) %>% mutate(forjoin = 1) %>% distinct()
+  ) -> VP_df2
+
+
+self2$add_VP(VP_df2)
+self2$n_filter_reduc()
+
+# self2$add_VP()
+
+
+ggplot()+
+  # geom_segment(data = forseg, aes(x = k2, xend = k2, y = lambda0, yend = lambda0max), lty = 3)+
+  # geom_segment(data = forseg, aes(x = k2, xend = k2max, y = lambda0, yend = lambda0), lty = 3)+
+  # geom_segment(data = tibble(k2_1), aes(x = k2_1, xend = k2_1, y = min(lambda0_1), yend = max(lambda0_1)), lty = 2, alpha  = 0.3)+
+  # geom_segment(data = tibble(lambda0_1), aes(x = min(k2_1), xend = max(k2_1), y = lambda0_1, yend = lambda0_1), lty = 2, alpha  = 0.3)+
+  # geom_point(data = ids %>% left_join(idoutput), aes(k2, lambda0))+
+
+  geom_rect(data= self2$filters_neg_above, aes(xmin = 0, xmax = k2, ymin = lambda0, ymax = Inf, fill = "Above", col = "Above"), alpha = 1)+
+  geom_rect(data= self2$filters_neg_below, aes(xmin = k2, xmax = Inf, ymin = 0, ymax = lambda0, fill = "Below", col = "Below"), alpha = 1)+
+  geom_rect(data = maybe,aes(xmin = k2min, xmax = k2max, ymin = lambda0min, ymax = lambda0max, fill = "Explored"), alpha = 0.6,  col = "black")+
+  # scale_x_continuous(breaks = k2_1)+
+  # scale_y_continuous(breaks = lambda0_1)+
+  # geom_segment(aes(x = 0, y = 0, xend = Inf, yend = 0, col = "Below"))+
+  # geom_point(data= self$filters_neg_below, aes(k2, lambda0, col = "Below"))+
+  # geom_point(data= self$filters_neg_above, aes(k2, lambda0, col = "Above"))+
+  scale_color_manual(values = c("red", "chocolate"))+
+  scale_fill_manual(values = c("red", "chocolate", "grey"))+
+  labs(col = "", fill = "", x = "K2", y = "lambda0")+
+  theme_bw()+
+  guides(col = F)+
+  geom_point(data = newVPs, aes(k2, lambda0))+
+  theme(line = element_blank())  -> plot4;plot4
+
+
+
+plot_grid(plot1, plot2, plot3, plot4)
+
+
+
+self2$compute_zone_maybe()
+maybe2 <- self2$zone_maybe
+maybe2 %>%
+  rowid_to_column("id") %>%
+  group_split(id) %>%
+  map(function(x){
+
+    difx <-  x$k2max -   x$k2min
+    dify <-  x$lambda0max -   x$lambda0min
+
+    crossing(k2 = seq(x$k2min, x$k2max, difx/40) %>% round(5),
+             lambda0 = seq(x$lambda0min, x$lambda0max, dify/40) %>% round(5)) %>%
+      mutate(bloc = x$id)
+
+  }) %>%
+  bind_rows() -> newVPs2;newVPs2
+
+self3 <- self2$clone(deep = T)
+
+newVPs2 %>%
+  mutate(forjoin = 1) %>%
+  left_join(
+    VP_df2 %>% select(-k2, -lambda0,-bloc ) %>% mutate(forjoin = 1) %>% distinct()
+  ) -> VP_df3
+
+
+self3$add_VP(VP_df3)
+
+self3$n_filter_reduc()
+self3$compute_zone_sure()
+
+
+
+plot5 <- ggplot()+
+
+  geom_rect(data= self3$filters_neg_above, aes(xmin = 0, xmax = k2, ymin = lambda0, ymax = Inf, fill = "Above", col = "Above"), alpha = 1)+
+  geom_rect(data= self3$filters_neg_below, aes(xmin = k2, xmax = Inf, ymin = 0, ymax = lambda0, fill = "Below", col = "Below"), alpha = 1)+
+  geom_rect(data = maybe2,aes(xmin = k2min, xmax = k2max, ymin = lambda0min, ymax = lambda0max, fill = "Explored"), alpha = 0.6,  col = "black")+
+  # scale_x_continuous(breaks = k2_1)+
+  # scale_y_continuous(breaks = lambda0_1)+
+  # geom_segment(aes(x = 0, y = 0, xend = Inf, yend = 0, col = "Below"))+
+  # geom_point(data= self$filters_neg_below, aes(k2, lambda0, col = "Below"))+
+  # geom_point(data= self$filters_neg_above, aes(k2, lambda0, col = "Above"))+
+  geom_rect(data = self3$zone_sure,aes(xmin = k2min, xmax = k2max, ymin = lambda0min, ymax = lambda0max, fill = "VPs"), alpha = 0.6,  col = "black")+
+  scale_color_manual(values = c("red", "chocolate"))+
+  scale_fill_manual(values = c("red", "chocolate", "grey", "darkgreen"))+
+  labs(col = "", fill = "", x = "K2", y = "lambda0")+
+  theme_bw()+
+  guides(col = F)+
+  theme(line = element_blank())+
+  geom_point(data = self3$poolVP, aes(k2, lambda0), col = "darkgreen")+
+  facet_zoom(xlim = c(1.15,1.26), ylim = c(0.19,0.225), split = F, zoom.size = 1);plot5
+
+VTplot <- self3$plot_VP()+
+  geom_point(data = self$targets %>% mutate(label = paste0("\n\nTime = ", time, "\n[", min, "-", max, "]\n\n")),
+             aes(time, min,col = label), size = 4)+
+  theme_bw()+
+  facet_wrap(~"Accepted VPs")+
+  labs(x = "Time (days)", y = "Tumor Volume (mm3)", col = "Targets"); VTplot
+
+
+prototiny <- tibble(protocol = "dose50", cmt = "tumVol", time = c(12,40), min = c(100, 431),
+                    max = c(100.5, 431.5))
+
+
+plothm <- ggplot()+
+  geom_rect(data = self3$zone_maybe, aes(xmin = k2min, xmax = k2max, ymin = lambda0min, ymax = lambda0max))+
+  # geom_segment(data = self3$zone_maybe, aes(x = k2min, xend = k2max, y = 1, yend = 1))+
+  # geom_rect(data = self3$zone_sure, aes(xmin = k2min, xmax = k2max, ymin = lambda0min, ymax = lambda0max), col = "darkgreen")+
+  theme_bw()+
+  facet_wrap(~"Possible zone of VPs kept in memory")
+
+
+plot_grid(plot_grid(plot1, plot2, plot3, plot4, labels = c("A", "B", "C", "D")), plot5, plot_grid(VTplot, ggplot(), labels = c("F", "G")), ncol = 1, rel_heights = c(2,1,1), labels = c("", "E") )
+
+
+
+
+seq(0,3,0.0015) %>% length() *
+seq(0,1.4,0.000820) %>% length()
+
+
+self4 <- self3$clone(deep = T)
+self3$compute_zone_maybe()
+maybe4 <- self3$zone_maybe
+
+maybe4 %>%
+  rowid_to_column("id") %>%
+  group_split(id) %>%
+  map(function(x){
+
+    difx <-  x$k2max -   x$k2min
+    dify <-  x$lambda0max -   x$lambda0min
+
+    crossing(k2 = seq(x$k2min, x$k2max, 0.0015) %>% round(5),
+             lambda0 = seq(x$lambda0min, x$lambda0max, 0.000820) %>% round(5)) %>%
+      mutate(bloc = x$id)
+
+  }) %>%
+  bind_rows() -> final_VPs
+
+
+self4$poolVP <- self4$poolVP %>% slice(0)
+
+
+
+final_VPs %>%
+  mutate(forjoin = 1) %>%
+  left_join(
+    VP_df2 %>% select(-k2, -lambda0,-bloc ) %>% mutate(forjoin = 1) %>% distinct()
+  ) -> VP_dffinal
+
+self4$add_VP(VP_dffinal)
+
+self4$compute_zone_sure()
+self4$compute_zone_maybe()
+
+
+
+plot6 <- ggplot()+
+
+  geom_rect(data= self4$filters_neg_above, aes(xmin = 0, xmax = k2, ymin = lambda0, ymax = Inf, fill = "Above", col = "Above"), alpha = 1)+
+  geom_rect(data= self4$filters_neg_below, aes(xmin = k2, xmax = Inf, ymin = 0, ymax = lambda0, fill = "Below", col = "Below"), alpha = 1)+
+  geom_rect(data = maybe4,aes(xmin = k2min, xmax = k2max, ymin = lambda0min, ymax = lambda0max, fill = "Explored"), alpha = 0.6)+
+  # scale_x_continuous(breaks = k2_1)+
+  # scale_y_continuous(breaks = lambda0_1)+
+  # geom_segment(aes(x = 0, y = 0, xend = Inf, yend = 0, col = "Below"))+
+  # geom_point(data= self$filters_neg_below, aes(k2, lambda0, col = "Below"))+
+  # geom_point(data= self$filters_neg_above, aes(k2, lambda0, col = "Above"))+
+  geom_rect(data = self4$zone_sure,aes(xmin = k2min, xmax = k2max, ymin = lambda0min, ymax = lambda0max, fill = "VPs"), alpha = 0.6)+
+  scale_color_manual(values = c("red", "chocolate"))+
+  scale_fill_manual(values = c("red", "chocolate", "grey", "darkgreen"))+
+  labs(col = "", fill = "", x = "K2", y = "lambda0")+
+  theme_bw()+
+  guides(col = F)+
+  theme(line = element_blank())+
+  geom_point(data = self4$poolVP, aes(k2, lambda0), col = "darkgreen")+
+  facet_zoom(xlim = c(1.15,1.26), ylim = c(0.15,0.27), split = F, zoom.size = 1)
+
+
+plot_grid(plot_grid(plot1, plot2, plot3, plot4), plot5, plot6, ncol = 1, rel_heights = c(2,1,1) )
+
+self4$poolVP
+self3$poolVP
+
+self4$poolVP
+poolVP$
 # prototiny <- tibble(protocol = "dose50", cmt = "tumVol", time = c(12,40), min = c(100, 431),
 #                     max = c(100.5, 431.5))
 
@@ -2206,10 +2445,9 @@ scale_fill_manual(values = c("red", "chocolate", "grey", "darkgreen"))
 
 plot_grid(plot_grid(plot1, plot2), plot3,ncol = 1 )
 
-  geom_segment(data = tibble(k2_1), aes(x = k2_1, xend = k2_1, y = min(lambda0_1), yend = max(lambda0_1)))+
-  geom_segment(data = tibble(lambda0_1), aes(x = min(k2_1), xend = max(k2_1), y = lambda0_1, yend = lambda0_1))+
-  geom_
-  theme_bw()
+
+
+
 
 
 
